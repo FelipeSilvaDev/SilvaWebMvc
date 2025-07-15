b7a3424f2f4da7e322fd75f4b6b24dd0c5b788a2
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-07-15 14:05:35 -0300

    CRUD Scaffolding

---
+++ SilvaWebMvc/Data/SilvaWebMvcContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SilvaWebMvc.Models
+{
+    public class SilvaWebMvcContext : DbContext
+    {
+        public SilvaWebMvcContext(DbContextOptions<SilvaWebMvcContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SilvaWebMvc.Models.Department> Department { get; set; }
+
+    }
+}
--- SilvaWebMvc/obj/Debug/netcoreapp2.1/SilvaWebMvc.AssemblyInfo.cs
+++ SilvaWebMvc/obj/Debug/netcoreapp2.1/SilvaWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SilvaWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4063844e586a6260ce836a2aee5a29d2a7276632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d22565960c26df234cae0301b4aa26e5539f76")]
 [assembly: System.Reflection.AssemblyProductAttribute("SilvaWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SilvaWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
