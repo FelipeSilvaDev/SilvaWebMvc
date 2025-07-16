ccd162e47fc858a4c8180d1ff1e616bea1e12131
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-07-16 14:32:15 -0300

    Criado o SellersController e corrigindo alguns nomes para portugues

---
+++ SilvaWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SilvaWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
