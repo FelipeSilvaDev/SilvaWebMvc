5af19c075a298df103895b60fe6c1dba60dd2f7f
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-07-22 15:38:30 -0300

    Operacoes assincronas usando Task(async e await)

--- SilvaWebMvc/Services/DepartmentService.cs
+++ SilvaWebMvc/Services/DepartmentService.cs
@@ -1,6 +1,8 @@
 ﻿using SilvaWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SilvaWebMvc.Services
 {
@@ -13,9 +15,9 @@ namespace SilvaWebMvc.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
